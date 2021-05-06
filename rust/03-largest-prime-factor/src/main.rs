52d7b829c6ef0137fbd9e6f7a33875f4ad09610f
James Hughes <james@pyrosoftsolutions.co.uk>
2021-05-06 23:30:56 +0100

    Solved problem 3 in rust

---
+++ rust/03-largest-prime-factor/src/main.rs
@@ -0,0 +1,31 @@
+use integer_sqrt::IntegerSquareRoot;
+
+fn main() {
+    let target: u64 = 600851475143;
+
+    
+    for i in (0..target.integer_sqrt()).rev().step_by(2)
+    {
+        if target % i == 0
+        {
+            if is_prime(i)
+            {
+                println!("{} is the largest prime factor of {}", i, target);
+                break;
+            }
+        }
+    }
+
+}
+
+fn is_prime(n: u64) -> bool
+{
+    for i in (3..(n).integer_sqrt()).step_by(2)
+    {
+        if n % i == 0
+        {
+            return false;
+        }
+    }
+    return true;
+}
